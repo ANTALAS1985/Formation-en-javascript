026bf6be6ce4d49291767bbf84e648d6ecf23df2
mustapha <mustapharaab@outlook.com>
2025-08-19 20:59:25 +0200

    syntaxe et variable en javascript

---
+++ 5-indentation en java script/javascript.js
@@ -0,0 +1,8 @@
+//je suis un commentaire mono ligne
+alert("une premiere instructiobn javasript");
+alert("une deuxieme instruction javascript");
+/*je suis un commentaire*/
+alert(5);
+alert(5.5);
+/*je suis un commentaire
+*multi-ligne*/
