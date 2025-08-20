05d32c8a793a225f197eb83438404a7971b162c3
mustapha <mustapharaab@outlook.com>
2025-08-20 23:21:56 +0200

    cours java script type de valeur

---
+++ 6-les variables/javascript.js
@@ -0,0 +1,13 @@
+var x = 25;
+x = x +5;
+var X = 100;
+
+var prenom = "mustapha"; nom ="mustapha"; departement = "val de marne";
+
+var age;
+age =39;
+
+var ville = "";
+ville = "vtry sur seine";
+
+var homme = true
---
+++ 7-Types de  valeurs/javascript.js
@@ -0,0 +1,36 @@
+// types number
+var x = 25;
+var y =-75;
+var z = 3.14;
+
+// types string
+var prenom  = "mustapha";
+var Nom = 'RAAB';
+var sexe ="je suis un homme";
+
+var departement = "je vie dans le \"83\"";
+var departement2 = 'je vie dans le "94"';
+var ville = 'j\'habite vtrysir seine';
+
+// types boolean
+var a = true, b = false;
+
+// types null
+var n = null;
+
+// types undefined
+var u = undefined;
+
+//types NaN
+var nn = NaN;
+
+// Remarque: la nouvemlle valeur ecrase l'ancienne.
+x = "quatre";
+
+// test des variable avec typeof en creant une alerte
+alert ("variable x : " + typeof (x) +
+"\nVariable y : " + typeof (y) +
+"\nVariable a : " + typeof (a) +
+"\nVariable n : " + typeof (n) +
+"\nVariable u : " + typeof (u) +
+"\nVariable nn : " + typeof (nn));
