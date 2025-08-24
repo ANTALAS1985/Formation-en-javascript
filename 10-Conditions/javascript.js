505b3619a6d587041108c2ce28764e4cc8be5931
mustapha <mustapharaab@outlook.com>
2025-08-24 16:51:17 +0200

    conditions cours java script

---
+++ 10-Conditions/javascript.js
@@ -0,0 +1,19 @@
+var x = 7,  y = 3;
+
+var vrai = x < y;
+var faux = 14 <= 7;
+
+var egalval = 4 == "4";
+
+var egaltype =  4 === "4";
+
+var difval = 4 != "4";
+
+var diftvaltype = 4 !== "4";
+
+alert ("vraistock: " + vrai +
+      "\nfauxstock: " + faux +
+      "\negalval : " + egalval +
+      "\negaltype : " + egaltype +
+      "\ndifval : " + difval +
+      "\ndifvaltype : " + diftvaltype);
