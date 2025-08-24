540b37344732897cd442c9f4c2c946ed2dcc7317
mustapha <mustapharaab@outlook.com>
2025-08-24 11:01:33 +0200

    concaténation entre chaine de caractére et nombre

---
+++ 9-Concaténation/concatenation entre un nombre et une chaine de caractére/javascript.js
@@ -0,0 +1,8 @@
+//Concaténation entre un nombre et une chaine de caractère
+var x = 4 + 2 + "1";
+var y = "1" + 2 + 4;
+var z = 2 + "un" + 4
+
+alert ("variable x :"  + x + typeof (x)+
+       "\nvariable y :" + y + typeof (y) +
+       "\nvariable z : " + z + typeof (z));
---
+++ 9-Concaténation/concaténation entre deux valeeur similaire/javascript.js
@@ -0,0 +1,12 @@
+//Concaténation.
+var prenom ='Mustapha', espace ='  ', nom ='RAAB';
+var moi = prenom + espace + nom;
+
+var toi = 'Rachid' + '  ' +  'zoubiri';
+
+var sport = 'courir';
+var hobbie = "j'aime" + espace + sport;
+
+alert("Contenu de la variable moi :" + moi +
+    "\nContenu de la variable toi :" + toi +
+    "\nContenu de la variable hobbie: " + hobbie);
