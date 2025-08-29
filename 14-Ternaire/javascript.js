6012b8f0f9d15765b1dbb6d4cf3b0400831a463f
mustapha <mustapharaab@outlook.com>
2025-08-29 22:43:20 +0200

    instruction swich et ternaire cours java script

---
+++ 14-Ternaire/javascript.js
@@ -0,0 +1,12 @@
+var heure = 19,  bon = "";
+
+bon = (heure <= 18)? "Bonjour" : "Bonsoir";
+alert (bon);
+
+if (heure <= 18){
+    bon = "bonjour"
+}
+else{
+    bon = "Bonsoir"
+}
+alert(bon);
---
+++ 15- l'istruction switch/javascript.js
@@ -0,0 +1,21 @@
+var heure = 12;
+
+switch( heure ){
+case 8 :
+    alert("il est 8 heure");
+    break;
+case 9 :
+    alert("il est 9 heure");
+    break;
+case 10 :
+    alert("il est 10 heure");
+    break;
+case 11:
+    alert("C'est 11 heure");
+    break;
+case 12:
+    alert("C'est 12")
+    break
+default:
+    alert("rien a afficher pour cette valeur");
+}
