7ef894f0164f59ace7dfcfe5409d0741631f5f1d
mustapha <mustapharaab@outlook.com>
2025-09-02 19:13:46 +0200

    les fonctions cours javascript

---
+++ 17- Fonctions/Fonction 1 multiplication/javascript.js
@@ -0,0 +1,5 @@
+function multiplication(x,y){ 
+    alert(x*y);
+}
+multiplication(5,10);
+multiplication(-4,60);
---
+++ 17- Fonctions/Fonction 2 return/javascript.js
@@ -0,0 +1,9 @@
+function multiplication(x,y){
+        return x*y;
+}
+
+var resultat1 = multiplication(5,10);
+var resultat2 = multiplication(-4,60);
+
+resultat1 +=2;
+alert(resultat1);
