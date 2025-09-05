95d262469dbd52556c7b0348a4076950c71de2da
mustapha <mustapharaab@outlook.com>
2025-09-05 20:41:40 +0200

    valeurs primitives et objets cours javascript

---
+++ 19-valeurs prémitives et objets/les objets 1/javascript.js
@@ -0,0 +1,9 @@
+var primitive = "je si=uis une valeur primitive";
+var type_prim = typeof(primitive);
+
+chaine = new String ("je suis un objet");
+type_chaine = typeof(chaine);
+
+alert ("type de primitive:" + type_prim +
+     "\ntype de chaine:" + type_chaine);
+
---
+++ 19-valeurs prémitives et objets/les objets 2/javascript.js
@@ -0,0 +1,13 @@
+var primitive = "je suis une valeur primitive";
+var longueur_prim = primitive.length;
+var maj_prim = primitive.toUpperCase();
+
+chaine = new String ("je suis un objet");
+var maj_chaine = chaine.toUpperCase();
+
+alert("longuer primitive:" + longueur_prim + 
+     "\nPrimitive en majuscules:" + maj_prim +
+     "\nObjet en majuscules:" + maj_chaine);
+
+
+
