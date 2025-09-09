292c3e2eec89a1afc51caae30ffcbd9c941e669e
mustapha <mustapharaab@outlook.com>
2025-09-09 20:30:52 +0200

    les methodes toUpperCase and toLowerCase

---
+++ 21-les methodes de l'objet string/Les methodes de l'objet string 1/javascript.js
@@ -0,0 +1,7 @@
+//*les methodes ToLowerCase() et ToUpperCase()
+var texte = "j'apprends le javascript";
+ var  textMin = texte.toLocaleLowerCase();
+ var  textMaj = texte.toUpperCase();
+
+ alert("texte en minuscule :" + textMin +
+       "\ntexte en majuscule : " + textMaj);
